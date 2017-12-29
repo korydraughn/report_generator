0da3fbfb0ab88da19d6dbf72edcbdd663b0420f9
Kory Dorrell Draughn <kdraughn@vdsinc.com>
2017-12-29 15:43:14 -0500

    Enhanced ui elements to help users understand what kind of information is being asked for. The inputs also help to enforce input formats which should decrease user errors.

--- src/student_info.js
+++ src/student_info.js
@@ -4,12 +4,44 @@ import {ContainerFluid, Row, Col} from './utils';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 
-function InputField(props)
+function TextField(props)
 {
   return (
     <div className="form-group">
       <label>{props.label}</label><br />
       <input type="text" className="form-control" />
+    </div>
+  );
+}
+
+function NumberField(props)
+{
+  return (
+    <div className="form-group">
+      <label>{props.label}</label><br />
+      <input type="number" className="form-control" />
+    </div>
+  );
+}
+
+function SelectBox(props)
+{
+  return (
+    <div className="form-group">
+      <label>{props.label}</label><br />
+      <select className="form-control">
+        {props.options.map((v, i) => <option key={i} value={v}>{v}</option>)}
+      </select>
+    </div>
+  );
+}
+
+function DateField(props)
+{
+  return (
+    <div className="form-group">
+      <label>{props.label}</label><br />
+      <input type="date" className="form-control" />
     </div>
   );
 }
@@ -43,8 +75,25 @@ export default class StudentInfo extends Component
         <fieldset>
           <legend>Student Demographics</legend>
           <Row>
-            <Col>{labels_lhs.map((l, i) => <InputField label={l} key={i} />)}</Col>
-            <Col>{labels_rhs.map((l, i) => <InputField label={l} key={i} />)}</Col>
+            <Col>
+              <TextField label="Student Name" />
+              <SelectBox label="Student Gender" options={["Male", "Female"]} />
+              <DateField label="Student Date of Birth" />
+              <TextField label="Student School" />
+            </Col>
+            <Col>
+              <Row>
+                <Col><DateField label="Date Administered - Start" /></Col>
+                <Col><DateField label="Date Administered - End" /></Col>
+              </Row>
+              <NumberField label="Age at Testing" />
+              <SelectBox label="Grade" options={[1, 2]} />
+            </Col>
+
+            <div hidden>
+              <Col>{labels_lhs.map((l, i) => <TextField label={l} key={i} />)}</Col>
+              <Col>{labels_rhs.map((l, i) => <TextField label={l} key={i} />)}</Col>
+            </div>
           </Row>
         </fieldset>
       </ContainerFluid>
