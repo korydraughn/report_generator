8938c1d2658889b8367668aedd1c86248e2ff019
Kory Dorrell Draughn <korydraughn@gmail.com>
2018-01-13 11:58:43 -0500

    Added code to expose history object.

--- src/report_form.js
+++ src/report_form.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 import StudentInfo from './student_info';
 import Notes from './notes';
 import Testing from './testing';
@@ -8,7 +9,13 @@ import {SingleElementContainer} from './utils';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-grid.css';
 
-export default props => {
+function onClick(e, props)
+{
+  e.preventDefault();
+  console.log(props);
+}
+
+export default withRouter(props => {
   const {data} = props;
   const {start_date, end_date} = data.tests;
   const {student, notes, tests} = props.handlers;
@@ -24,12 +31,14 @@ export default props => {
       <Testing data={data.tests.data} {...tests} />
 
       <SingleElementContainer>
-        <Link to="/report">
-          <button type="button" className="btn btn-lg btn-primary float-right">
-            Generate Report
-          </button>
+        <Link
+          className="btn btn-lg btn-primary float-right"
+          role="button"
+          to="/report"
+          onClick={e => onClick(e, props)}>
+          Generate Report
         </Link>
       </SingleElementContainer>
     </div>
   );
-};
+});
